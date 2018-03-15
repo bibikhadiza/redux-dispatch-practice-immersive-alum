a9e08bb375f2fabaaf206f91d06795556c6dcd8f
Bibi Khadiza <bibi_khadiza_accenture@whirlpool.com>
2018-03-15 15:59:35 -0400

    Completed lab

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,30 @@
 export let state;
 
-
-export function managePets(){
+export function managePets(state = {pets: []}, action){
+  switch (action.type) {
+    case ("ADD_PET"):
+      if(action.pet.name && action.pet.species && action.pet.id) {
+        let stateCopy = Object.assign({}, state);
+        stateCopy.pets.push(action.pet);
+        return stateCopy;
+      }
+    case ("REMOVE_PET"):
+      if(action.id){
+        let stateCopy = Object.assign({}, state);
+        stateCopy.pets = stateCopy.pets.filter(e => e.id != action.id);
+        return stateCopy;
+      }
+    default:
+      return state;
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action);
+  render();
 }
 
 export function render(){
-
+  let container = document.getElementById('container');
+  state.pets.forEach( e => container.innerHTML = `<ul><li>${e.name}</li></ul>`);
 }
